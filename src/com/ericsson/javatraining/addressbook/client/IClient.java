1e99f9d68cce6ec1474613fcabda872d7e0d3bcb
eyuaqiu <drake.qiu@ericsson.com>
2012-05-03 13:17:57 +0800

    [other] refactor
    Change-Id: I607e6417c094026c4c530ce74e478ce779887776

--- src/com/ericsson/javatraining/addressbook/client/IClient.java
+++
@@ -1,7 +0,0 @@
-package com.ericsson.javatraining.addressbook.client;
-
-public interface IClient {
-	public void start();
-	
-	final static String STARLINE = "****************************************************************************";
-}
