f38f0045238d2162b97b78842be08c2461dd7cd9
eyuaqiu <drake.qiu@ericsson.com>
2012-05-08 10:27:43 +0800

    [other] modify
    Change-Id: Id7c15296ea2bbc43c67374381d1997a8ba2f7afa

--- src/main/java/com/ericsson/javatraining/addressbook/service/AddressBookServiceImpl.java
+++ src/main/java/com/ericsson/javatraining/addressbook/service/AddressBookServiceImpl.java
@@ -13,23 +13,30 @@ public class AddressBookServiceImpl implements IAddressBookService {
 	public List searchAddress(List addressBookList, String number) {
 		// TODO Auto-generated method stub
 		if (addressBookList == null) {
+
 			return null;
 		}
 		ArrayList resultList = new ArrayList();
 		Iterator it = addressBookList.iterator();
-		while (it.hasNext()) {
-			AddressItem item = (AddressItem) it.next();
-			String phoneNum = item.getPhoneNum();
-			if (phoneNum != null && !("".equals(phoneNum))
-					&& phoneNum.contains(number)) {
-				resultList.add(item);
+		if (number == null || "".equals(number)) {
+
+			return addressBookList;
+		} else {
+			while (it.hasNext()) {
+				AddressItem item = (AddressItem) it.next();
+				String phoneNum = item.getPhoneNum();
+				if (phoneNum.contains(number)) {
+					resultList.add(item);
+				}
 			}
+
 		}
+
 		return resultList;
 	}
 
 	@Override
-	public void addAddress(List addressBookList, AddressItem item) {
+	public synchronized void addAddress(List addressBookList, AddressItem item) {
 		// TODO Auto-generated method stub
 		if (item != null) {
 			addressBookList.add(item);
