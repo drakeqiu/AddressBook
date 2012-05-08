b8b67399a8370d9a0a882a2e347a741b76864908
eyuaqiu <drake.qiu@ericsson.com>
2012-05-08 19:49:24 +0800

    [other].
    Change-Id: I133c9de6389c181add740fef3fa0930037ac9d55

--- src/main/java/com/ericsson/javatraining/addressbook/util/StringUtil.java
+++ src/main/java/com/ericsson/javatraining/addressbook/util/StringUtil.java
@@ -3,7 +3,7 @@ package com.ericsson.javatraining.addressbook.util;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class StringUtil {
+public final class StringUtil {
 	public static boolean regmatch(String regex, String string){
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(string);
