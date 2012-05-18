9dea0e99f47f0371f99e461fd7ce989c27f2c3bc
eyuaqiu <drake.qiu@ericsson.com>
2012-05-18 17:01:06 +0800

    [other] add junit use
    Change-Id: Ied6c49b0725785b8c0a3fa895bdbcbd920e4a9e9

---
+++ src/test/java/com/ericsson/javatraining/addressbook/action/AddAddressActionTest.java
@@ -0,0 +1,77 @@
+package com.ericsson.javatraining.addressbook.action;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AddAddressActionTest {
+	
+	private AbstractAddressAction action;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		action = new AddAddressAction();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAction() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetAddressBook() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testSetAddressBook() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAbstractAddressAction() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
